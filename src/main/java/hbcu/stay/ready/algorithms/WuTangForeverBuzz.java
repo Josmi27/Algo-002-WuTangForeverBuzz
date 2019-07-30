aa39696f8e82d404406c8c56abbcd4e8e7392e35
Joshua <jds13245@hotmail.com>
2019-07-30 11:05:04 -0400

    Successfully completed the test case

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -6,20 +6,18 @@ public class WuTangForeverBuzz {
         String wuwu = "";
         int input = 16;
 
-        for (int i = 1; i < 16; i++) {
-            if(i % 3 == 0) {
+        for (int i = 1; i < input; i++) {
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                wuwu += "WuTang Forever";
+            }
+            else if(i % 3 == 0) {
                 wuwu += "Wu\n";
             }
             else if(i % 5 == 0) {
                 wuwu += "Tang\n";
             }
             // Problems checking this condition.
-            else if((i % 3 == 0) && (i % 5 == 0)) {
-                if(i+1 > input) {
-                    wuwu += "WuTang Forever";
-                }
-                wuwu += "WuTang Forever\n";
-            }
+
             else {
                 wuwu += i + "\n";            }
         }
