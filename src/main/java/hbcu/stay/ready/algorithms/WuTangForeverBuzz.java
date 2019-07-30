dcb74fff09c845d0bd25b64e91269ab015e5cb68
Joshua <jds13245@hotmail.com>
2019-07-30 03:14:48 -0400

    First attempt at solving test case

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,26 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        String wuwu = "";
+        int input = 16;
+
+        for (int i = 1; i < 16; i++) {
+            if(i % 3 == 0) {
+                wuwu += "Wu\n";
+            }
+            else if(i % 5 == 0) {
+                wuwu += "Tang\n";
+            }
+            // Problems checking this condition.
+            else if((i % 3 == 0) && (i % 5 == 0)) {
+                if(i+1 > input) {
+                    wuwu += "WuTang Forever";
+                }
+                wuwu += "WuTang Forever\n";
+            }
+            else {
+                wuwu += i + "\n";            }
+        }
+        return wuwu;
     }
 }
